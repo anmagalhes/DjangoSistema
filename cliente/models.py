409cfb7bc06f2ab42db440454a33b0d3c50f40e2
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2023-02-10 22:14:48 -0300

    Atualização

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-#class Cliente(model.model)
+class Sexo(models.Model):
+    SEXO_CHOICES = (
+    ),
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=80)
+    numeroCPF = models.CharField(max_length=15)
+    rg = models.CharField(max_length=15)
+    datanascimento = models.DateField(
+        null=True,
+        blank=True, 
+    )
+
+
