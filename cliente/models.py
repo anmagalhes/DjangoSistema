56bce8bd676c1d8d7bddcaffa17e494bde8d62dc
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2023-02-15 20:59:15 -0300

    Atualização

--- cliente/models.py
+++ cliente/models.py
@@ -12,14 +12,20 @@ class Cliente(models.Model):
         ("I","Inativo"),
     )
     
-    nome = models.CharField(max_length=80)
-    numeroCPF = models.CharField(max_length=15)
-    rg = models.CharField(max_length=15)
+    nome = models.CharField(
+        'Nome do cliente',
+        max_length=80)
+    CPF = models.CharField(max_length=15)
+    rg = models.CharField(
+        'RG',
+        max_length=15)
     datanascimento = models.DateField(
+        'Data de nascimento',
         null=True,
         blank=True, 
     )
     sexo =  models.CharField(
+        'Sexo',
         max_length=1,
         choices=SEXO_CHOICES,
         null=False,
@@ -27,18 +33,27 @@ class Cliente(models.Model):
         default='M',
     )
     profissao = models.CharField(
+        'Profissão do Cliente',
         max_length=80,
-
+        blank=True,
+        null=True,
     )
     email = models.EmailField(max_length=50)
     #uf = models.CharField(max_length=80)
     #cidade = models.CharField(max_length=80)
     #bairro = models.CharField(max_length=80)
     #logradouro = models.CharField(max_length=80)
-    fixo = models.CharField(max_length=12)
-    celular = models.CharField(max_length=11)
-    what = models.CharField(max_length=11)
+    fixo = models.CharField(
+        'Numero de fixo com o (DDD)',
+        max_length=12)
+    celular = models.CharField(
+        'Numero de celular com o (DDD)',
+        max_length=11)
+    what = models.CharField(
+        'Contato do Whatzapp',
+        max_length=11)
     situacao =  models.CharField(
+        'Situação do cliente',
         max_length=1,
         choices=STATUS_SITUACAO,
         null=False,
@@ -47,7 +62,7 @@ class Cliente(models.Model):
     )
 
     def __str__(self) -> str:
-        return self.Cliente
+        return self.nome
 
 
 
