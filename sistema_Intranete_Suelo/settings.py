df1bd61f07986de695b4d86dfeac131f13547162
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2023-02-20 17:22:33 -0300

    Atualização

--- sistema_Intranete_Suelo/settings.py
+++ sistema_Intranete_Suelo/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'static/user'
 
 
 # Default primary key field type
