521d9bf870e5ac129858fe6c78610c4ea720d75c
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2023-02-21 13:14:23 -0300

    Atualização 21.02.2023

---
+++ cliente/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from cliente.models import *
+
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ('__all__')
--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cliente.models import *
+from cliente.forms import *
+import datetime
 
-# Create your views here.
+def cadastrar_cliente(request):
+    form = ClienteForm()
+
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("cadastrar_cliente")
+
+    context = {
+
+        "nome_pagina": "Cadastrar Cliente",
+        "form": form
+    }
+
+    return render(request, "cadastrar_cliente.html", context)
