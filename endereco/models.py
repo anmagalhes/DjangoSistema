7bd5cc2a1387bbb4848b22fc4d0a8bd3b5c3b8b1
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2023-02-22 19:17:29 -0300

    Atualização

--- endereco/models.py
+++ endereco/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+LOGRADOURO_CHOICES = [
+                ('Rua', 'Rua'),
+                ('Avenida', 'Avenida'),
+                ('Praca', 'Praca'),
+                ('Estrada', 'Estrada'),
+                ('Travessa', 'Travessa'),
+                ('Viela', 'Viela'),
+                ('Passagem', 'Passagem'),
+    ]
+
 # Create your models here.
+class Endereco(models.Model):
+    pass
