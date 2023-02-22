a899b9cc8c8bcdd5cfb67c7c58daaaece3392ed4
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2023-02-22 19:47:35 -0300

    Atualização

--- endereco/models.py
+++ endereco/models.py
@@ -11,6 +11,35 @@ LOGRADOURO_CHOICES = [
     ('Passagem', 'Passagem'),
 ]
 
+ESTADO_CHOICES = [
+        (u'AC', u'AC'),
+        (u'AP', u'AM'),
+        (u'AM', u'AM'),
+        (u'BA', u'BA'),
+        (u'CE', u'CE'),
+        (u'DF', u'DF'),
+        (u'ES', u'ES'),
+        (u'GO', u'GO'),
+        (u'MA', u'MA'),
+        (u'MT', u'MT'),
+        (u'MS', u'MS'),
+        (u'MG', u'MG'),
+        (u'PA', u'PA'),
+        (u'PB', u'PB'),
+        (u'PR', u'PR'),
+        (u'PE', u'PE'),
+        (u'PI', u'PI'),
+        (u'RJ', u'RJ'),
+        (u'RN', u'RN'),
+        (u'RS', u'RS'),
+        (u'RO', u'RO'),
+        (u'RR', u'RR'),
+        (u'SC', u'SC'),
+        (u'SP', u'SP'),
+        (u'SE', u'SE'),
+        (u'TO', u'TO'),
+    ]
+
 # Create your models here.
 class Endereco(models.Model):
     Logradouro = models.CharField(
@@ -37,4 +66,38 @@ class Endereco(models.Model):
         max_length=7,
         null=False,
         blank=False,
+    )
+
+    Complem = models.CharField(
+        'Complemento',
+        help_text='Complemento Casa ou Apptamento',
+        max_length=7,
+        null=False,
+        blank=False,
+    )
+
+    Bairro = models.CharField(
+        'Bairro',
+        help_text='Bairro da Residencia',
+        max_length=70,
+        null=False,
+        blank=False,
+    )
+
+    Cidade = models.CharField(
+        'Cidade',
+        help_text='Cidade da Residencia',
+        max_length=70,
+        null=False,
+        blank=False,
+    )
+
+    Estado = models.CharField(
+        'Estado',
+        help_text='Nome do Estado',
+        max_length=9,
+        choices= ESTADO_CHOICES,
+        null=False,
+        blank=False,
+        default='Rua',
     )
